147f88784d20445446a2c19aea028fcceb614e72
Petrouy <petros3014@gmail.com>
2021-12-13 23:09:32 +0100

    Přidejte soubory projektu.

---
+++ Aplikace API NASA/Aplikace API NASA/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Aplikace_API_NASA
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        static async Task Main(string[] args)
+        {
+            string API_KEY = "76j9PIp3V02cbCvlZUHtQneCn2vHChnhcIEx25c2";
+
+            using (var hc = new HttpClient())
+            {
+                var response = await hc.GetAsync("https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=API_KEY");
+                return response;
+            }
+        }
+    }
+}
